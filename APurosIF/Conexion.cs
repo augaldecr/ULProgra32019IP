3c4f4775839eaaee0dc13c24ffdf63a8aa2f2432
augaldecr <augaldecr@gmail.com>
2019-01-31 19:25:17 -0600

    Creacion de clase conexion

---
+++ APurosIF/Conexion.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+namespace APurosIF
+{
+    public enum Estados
+    {
+        Ïnactiva,
+        Activa,
+        Pausada,
+        Cancelada
+    }
+
+    public class Conexion
+    {
+        public Estados Estado;
+
+        public Conexion()
+        {
+            this.Estado = Estados.Ïnactiva;
+        }
+
+        public void Conectar()
+        {
+            this.Estado = Estados.Activa;
+        }
+
+        public void Desconectar()
+        {
+            this.Estado = Estados.Ïnactiva;
+        }
+
+
+        public void Pausar()
+        {
+            this.Estado = Estados.Pausada;
+        }
+
+        public void Cancelar()
+        {
+            this.Estado = Estados.Ïnactiva;
+        }
+    }
+}
---
+++ APurosIF/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace APurosIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conekcion = new Conexion();
+
+            if (conekcion.Estado != Estados.Activa)
+            {
+                conekcion.Conectar();
+            }
+
+            if (conekcion.Estado != Estados.Cancelada)
+            {
+                conekcion.Cancelar();
+            }
+
+            if (conekcion.Estado != Estados.Ïnactiva)
+            {
+                conekcion.Cancelar();
+            }
+
+            if (conekcion.Estado != Estados.Pausada)
+            {
+                conekcion.Desconectar();
+            }
+
+            Console.WriteLine(conekcion.Estado);
+            Console.ReadKey();
+        }
+    }
+}
